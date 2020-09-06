3c416001b8a48025c722f8885c29ddab153278a8
Christofferck <70515469+Christofferck@users.noreply.github.com>
2020-09-06 20:35:19 +0200

    Add files via upload

---
+++ Assignment2/jsfTextUtils/jsTextUtils.js
@@ -0,0 +1,25 @@
+/* opgave 2.1
+var s = prompt('');
+function first(s) {
+	return s[0];
+}
+document.write(first(s))
+*/
+
+
+/* Opgave 2.2
+var s = prompt('');
+function last(s) {
+	return s[s.length-1];
+}
+document.write(last(s))
+*/
+
+/* Opgave 2.3
+var s = prompt('');
+function middle(s) {
+	return s.slice(1,-1);
+}
+document.write(middle(s))
+
+*/
